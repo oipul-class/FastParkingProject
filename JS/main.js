2f8ef8330e56fd3d8abeb47ee3d166aae98eea74
GABRIEL MOCHIUTE DE ALMEIDA <20185446@CFP127ALUNO.LOCAL>
2020-12-14 16:46:43 -0300

    Update da saida

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,20 @@
 'use strict';
 
-const entradaDados = () => {
-    const url = '../api/index.php/estadia';
 
-    fetch(url).then(response => response.json()).then(data => preenche(data));
 
+
+const entradaDados = (option) => {
+    let url = "";
+    switch(option){
+        case("entrada"):
+            url = '../api/index.php/estadia';
+            fetch(url).then(response => response.json()).then(data => preencher(data));
+            break;
+        case("saida"):
+            url = '../api/index.php/estadia';
+            fetch(url).then(response => response.json()).then(data => saida(data));
+            break;
+    }
 }
 
 const preencherEntrada = (dados) => {
@@ -26,8 +36,39 @@ const preencherEntrada = (dados) => {
     return div;
 }
 
+const preencherSaida = (dados) => {
+    const div = document.createElement('div');
+    // div.classList.add('container1');
 
-const preenche = (dados) => {
+    const data = new Date();
+    let hora = data.getHours() -1;
+    const horaDeEntrada = dados.horaDaEntrada.split(':', 1);
+
+    const resultado = 12 + ((hora - horaDeEntrada) * 6);
+
+    console.log(dados);
+    if(dados.horaDaSaida == null){
+        div.innerHTML = `
+        <div class="saidaCardContainer">
+            <div class="saidaNome">
+                <h1>${dados.placa}</h1>
+                <h3>${dados.nome}</h3>
+                <h2>Horário de Entrada:${dados.horaDaEntrada}</h2>
+                <h6>Id:${dados.idEstadia}</h6>
+            </div>
+            <div class="saidaPreco">
+                <h1>R$:` + resultado + `,00</h1>
+                <button>Calcular saída</button>
+            </div>
+        </div>`
+    }
+;
+
+    return div;
+}
+
+
+const preencher = (dados) => {
     const dadosJson = dados.estadias;
     const container = document.querySelector('.containerCard');
     
@@ -36,4 +77,15 @@ const preenche = (dados) => {
     });
 }
 
-entradaDados();
+
+const saida = (dados) => {
+    const dadosJson = dados.estadias;
+    const container = document.querySelector('#saidaCards');
+    
+    dadosJson.forEach(element => {
+        container.appendChild(preencherSaida(element));
+    });
+}
+
+entradaDados('entrada');
+entradaDados('saida');
